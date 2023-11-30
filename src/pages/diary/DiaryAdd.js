e2bfe3598b24442eaa2d7fe88c931308fa6d9155
devgreact <dev.greact@gmail.com>
2023-11-30 18:03:32 +0900

    feat: router 기본 적용

---
+++ src/pages/diary/DiaryAdd.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+import Footer from "../../components/Footer";
+
+const DiaryAdd = () => {
+  return (
+    <>
+      <Header>My Diary 추가</Header>
+      <Main>
+        <h2 style={{ textAlign: "center", color: "red" }}>다이어리 추가화면</h2>
+      </Main>
+      <Footer>
+        <p>Coprytight 2023. 다이어리 추가화면</p>
+      </Footer>
+    </>
+  );
+};
+
+export default DiaryAdd;
